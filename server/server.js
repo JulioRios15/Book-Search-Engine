259087cbcaed3bed80d29735e43b5ce9147e62a4
JulioRios15 <89167159+JulioRios15@users.noreply.github.com>
2022-01-22 20:46:32 -0400

    comment
    comment for debuging

--- server/server.js
+++ server/server.js
@@ -21,16 +21,18 @@ const startApolloServer = async () => {
 }
 
 startApolloServer();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
 app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  //console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
