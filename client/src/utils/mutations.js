1c976f7eba1362b573762e3c5824064ac63c5ab1
JulioRios15 <89167159+JulioRios15@users.noreply.github.com>
2022-01-22 20:48:45 -0400

    client schemas
    implement query and mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,92 @@
 import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login(
+    $email: String!
+    $password: String!
+  ) {
+    login(
+      email: $email
+      password: $password
+    ) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser(
+    $username: String!
+    $email: String!
+    $password: String!
+  ) {
+    addUser(
+      username: $username
+      email: $email
+      password: $password
+    ) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+mutation saveBook(
+  $userId: ID!
+  $authors: [String]!
+  $description: String!
+  $title: String!
+  $bookId: String!
+  $image: String!,
+  $link: String!
+  ) {
+    saveBook(
+      userId: $userId
+      authors: $authors
+      description: $description
+      title: $title
+      bookId: $bookId
+      image: $image 
+      link: $link
+      ) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        description
+        image
+        link
+        title
+        authors
+    }
+  }
+}
+`
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        description
+        image
+        link
+        title
+        authors
+      }
+    }
+  }
+`;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -14,6 +14,8 @@ export const QUERY_ME = gql`
         link
         title
         authors
+      }
     }
   }
+
 `;
